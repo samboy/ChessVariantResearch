62ae073ac38bace4aed3a6f47c5e71818e9ab41a
Sam Trenholme <gravatar@samiam.org>
2021-02-15 02:48:15 -0800

    chessClient: Now works, generates sample randomized games

--- Lua-client/chessClient.lua
+++ Lua-client/chessClient.lua
@@ -1,9 +1,15 @@
 -- Placed in the public domain 2021 by Sam Trenholme
 -- This is a lunacy (Lua + Steve Donovan's popen lib) implementation of
 -- a simple Chess/Chess variants client
+--
+-- This client is a “randomized” version of Fairy-Stockfish:  It looks
+-- at the top MultiPV number of moves (default: 3), and chooses one within
+-- 50 centipawns of what it thinks is the best move at random.
+--
 -- This client requires the Fairy-Stockfish program to be installed
 -- and available with the name fairy-stockfish-largeboard_x86-64
 -- (if it has another name, change "ChessEngine" below)
+--
 -- See https://github.com/ianfab/Fairy-Stockfish for the Chess engine
 ChessEngine = "fairy-stockfish-largeboard_x86-64"
 -- This is the number of lines we look at and consider for our next move
@@ -11,13 +17,17 @@ MultiPV = 3
 -- The name of the variant we will look at.  This needs to be a variant
 -- Fairy-Stockfish supports
 variantName = "capablanca"
+-- variantName = "chess"
 -- The opening setup (or position) we will play from in the game
 -- This is an argument given to the Fairy-Stockfish "setboard" command
 variantFEN = "ranbqkbncr/pppppppppp/10/10/10/10/PPPPPPPPPP/RANBQKBNCR " ..
              "w KQkq - 0 1"
+-- variantFEN = false -- Use default opening setup for variant
 -- After this many plies are searched, decide on a move to make
-searchPly = 7
+searchPly = 17
 -- Here be dragons below
+
+if searchPly < 7 then print("searchPly too small") os.exit(1) end
 
 ----------------------- rStrSplit() -----------------------
 -- This does a simple split for a given string, useful for simple CSV
@@ -49,7 +59,9 @@ w,r = popen.popen2(ChessEngine)
 w:write("setoption name MultiPV value " .. tostring(MultiPV) .. "\n")
 w:write("xboard\n")
 w:write("variant " .. variantName .. "\n")
-w:write("setboard " .. variantFEN .. "\n")
+if variantFEN then
+  w:write("setboard " .. variantFEN .. "\n")
+end
 w:write("d\n")
 w:flush()
 lineFromEngine = ""
@@ -64,7 +76,9 @@ game = ""
 gamePly = 1
 movelist = {}
 movevalue = {}
-maxvalue = -100000
+maxvalue = -10000000
+acceptMove = false
+bestMove = ""
 while true do
   lineFromEngine = r:read()
   local fields = rStrSplit(lineFromEngine,' ')
@@ -85,14 +99,15 @@ while true do
   end
   if #fields > 0 and string.match(fields[1],'Error') then 
     game = game .. lineFromEngine
+    w:write("d\n")
+    w:flush()
+    lineFromEngine = ""
+    while not string.match(lineFromEngine,'^Key') do
+      lineFromEngine = r:read()
+      print(lineFromEngine)
+    end
     print(game)
-w:write("d\n")
-w:flush()
-lineFromEngine = ""
-while not string.match(lineFromEngine,'^Key') do
-  lineFromEngine = r:read()
-  print(lineFromEngine)
-end
+    print("\nERROR")
     os.exit(1) -- Ended with Error
   end
   if #fields > 7 then
@@ -100,21 +115,41 @@ end
     local value = tonumber(fields[2])
     local time = tonumber(fields[3])
     local move = fields[8]
-    if ply == searchPly and movevalue[move] == nil then
+    if ply == searchPly - 5 then acceptMove = true end
+    if ply == searchPly and movevalue[move] == nil and acceptMove then
       movelist[#movelist + 1] = move
     end
     if ply == searchPly then
       movevalue[move] = value
+      bestMove = move
       if value > maxvalue then maxvalue = value end
     end
     if ply > searchPly and #movelist > 0 then
-      local thisValue = -100000
+      local thisValue = -10000000
       local toMove = ""
-      while thisValue < maxvalue - 50 do
+      local limit = 20
+      while thisValue < maxvalue - 50 and limit > 0 do
         toMove = movelist[math.random(#movelist)]
 	if movevalue[toMove] ~= nil then
 	  thisValue = movevalue[toMove]
 	end
+	limit = limit - 1
+      end
+      if limit < 2 then
+        toMove = bestMove
+      end
+      if #toMove < 1 then
+        game = game .. lineFromEngine
+        w:write("d\n")
+        w:flush()
+        lineFromEngine = ""
+        while not string.match(lineFromEngine,'^Key') do
+          lineFromEngine = r:read()
+          print(lineFromEngine)
+        end
+        print(game)
+        print("\nERROR")
+        os.exit(1)
       end
       game = game .. toMove .. " "
       print(gamePly, toMove)
@@ -125,7 +160,8 @@ end
       lineFromEngine = r:read()
       movelist = {}
       movevalue = {}
-      maxvalue = -100000
+      maxvalue = -10000000
+      acceptMove = false
     end
   end
 end
