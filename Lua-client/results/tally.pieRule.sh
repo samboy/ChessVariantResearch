1389943003047793103ad433ded996c344b6e167
Sam Trenholme <maradns@samiam.org>
2021-03-01 15:20:23 -0800

    tally.pieRule: Add overall results
    100% means White always wins
    50% is a combination of all draws to White-wins-half+Black-wins-half
    0% is Black always wins

--- Lua-client/results/tally.pieRule.sh
+++ Lua-client/results/tally.pieRule.sh
@@ -4,11 +4,14 @@
 # win, = is draw
 
 awk '{a=$(NF-1);
-if(a ~ /White/){white[$2]++}
+if(a ~ /White/){white[$2]++ ; score[$2]++}
 if(a ~ /Black/){black[$2]++}
-if(a ~ /1\/2/){draw[$2]++}}
+if(a ~ /1\/2/){draw[$2]++ ; score[$2] += 0.5 }
+total[$2]++
+}
 END {
 for(a in white){print a " +"white[a]}
 for(a in black){print a " -"black[a]}
 for(a in draw) {print a " ="draw[a]}
+for(a in score) {print a " result: " (score[a] / total[a]) * 100 "%"}
 }' | sort
