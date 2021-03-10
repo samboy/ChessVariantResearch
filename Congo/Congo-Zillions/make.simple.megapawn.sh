591998bd226803e817a21942eee00a996a0e7c4b
Sam Trenholme <gravatar@samiam.org>
2021-03-10 00:38:31 -0800

    make.simple.megapawn.sh: Let’s have version with full river

--- Congo/Congo-Zillions/make.simple.megapawn.sh
+++ Congo/Congo-Zillions/make.simple.megapawn.sh
@@ -8,12 +8,14 @@ cd Congo
 head -562 Congo.zrf > foo
 cp Congo.zrf Congo.save
 mv foo Congo.zrf
+cp Congo.zrf Congo-noislands.zrf
 
 # Make A4/B4/F4/G4 "islands" where one does not drown
 patch < ../Congo.zrf.islands.patch
 
 # Add a variant with a different setup and a very powerful "Megapawn"
 cat ../megapawn.txt >> Congo.zrf
+cat ../megapawn.txt >> Congo-noislands.zrf
 
 mv Congo.zrf congo-megapawn.zrf
 mv Congo.save Congo.zrf
