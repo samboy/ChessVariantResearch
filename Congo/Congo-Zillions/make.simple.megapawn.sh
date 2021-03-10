d8dc5098fafab8212c0403de5a433a094fd458e3
Sam Trenholme <gravatar@samiam.org>
2021-03-10 00:31:04 -0800

    megapawn: Add a bunch more variants

--- Congo/Congo-Zillions/make.simple.megapawn.sh
+++ Congo/Congo-Zillions/make.simple.megapawn.sh
@@ -2,8 +2,11 @@
 
 # Unzip needed to extract the congo.zip file
 
+#rm -fr Congo congo-megapawn
 unzip congo.zip
 cd Congo
+head -562 Congo.zrf > foo
+mv foo Congo.zrf
 
 # Make A4/B4/F4/G4 "islands" where one does not drown
 patch < ../Congo.zrf.islands.patch
