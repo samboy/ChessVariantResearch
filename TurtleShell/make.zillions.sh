a1d4a7f94cb2108409f8b257035d6de6b2b7f4cf
Sam Trenholme <git@samiam.org>
2022-04-01 18:57:31 -0700

    TurtleShell: Make shell script to zip up Zillions release

---
+++ TurtleShell/make.zillions.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+# Make a release of the Zillions version of Turtle Shell Chess
+
+# Requires: unix2dos zip
+# Optional: advdef (advzip, etc.)
+
+HERE=$( /bin/pwd )
+if [ ! -e "$HOME/tmp" ] ; then
+  mkdir $HOME/tmp
+  echo Directory $HOME/tmp made
+fi
+if [ ! -e Zillions ] ; then
+  echo Zillions not found\; aborting
+  exit 1
+fi
+
+mkdir $HOME/tmp/$$
+VERSION=$( date +%Y-%m-%d-%H%M )
+DIR=$HOME/tmp/$$/TurtleShellZillions-$VERSION
+mkdir $DIR
+mkdir $DIR/images
+cp Zillions/images/*bmp $DIR/images
+cp Zillions/TurtleShell.zrf $DIR
+cp Zillions/TurtleShell.txt $DIR
+echo Version $VERSION >> $DIR/TurtleShell.txt
+echo \; Version $VERSION >> $DIR/TurtleShell.zrf
+unix2dos $DIR/TurtleShell.txt
+unix2dos $DIR/TurtleShell.zrf
+cd $HOME/tmp/$$
+zip -r TurtleShellZillions-${VERSION}.zip TurtleShellZillions-$VERSION
+advzip -z -4 TurtleShellZillions-${VERSION}.zip
+cp TurtleShellZillions-${VERSION}.zip $HERE
+echo TurtleShellZillions-${VERSION}.zip made
+rm -fr $HOME/tmp/$$
