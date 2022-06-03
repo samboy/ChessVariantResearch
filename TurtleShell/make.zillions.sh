02a7ce1b25878016ce9164f615580d40d4183ccc
Sam Trenholme <git@samiam.org>
2022-06-03 16:40:34 -0700

    make.zillions.sh: Make smaller version of Zillions file w/o PDF

--- TurtleShell/make.zillions.sh
+++ TurtleShell/make.zillions.sh
@@ -25,13 +25,21 @@ cp Zillions/images/TurtleShellChess/*bmp $DIR/images/TurtleShellChess/
 cp Zillions/TurtleShell.zrf $DIR
 cp Zillions/TurtleShell.txt $DIR/ReadMe.txt
 cp TurtleShellRules*pdf $DIR/TurtleShell.pdf
+cp TurtleShellRules.txt $DIR/
+cp TurtleShell-algebraic-zones-small.png $DIR/TurtleShell.png
 echo Version $VERSION >> $DIR/TurtleShell.txt
 echo \; Version $VERSION >> $DIR/TurtleShell.zrf
 unix2dos $DIR/TurtleShell.txt
 unix2dos $DIR/TurtleShell.zrf
+unix2dos $DIR/TurtleShellRules.txt
 cd $HOME/tmp/$$
 zip -r TurtleShellZillions-${VERSION}.zip TurtleShellZillions-$VERSION
 advzip -z -4 TurtleShellZillions-${VERSION}.zip
 cp TurtleShellZillions-${VERSION}.zip $HERE
+rm $DIR/*pdf
+zip -r TurtleShellZillions-Small-${VERSION}.zip TurtleShellZillions-$VERSION
+advzip -z -4 TurtleShellZillions-Small-${VERSION}.zip
+cp TurtleShellZillions-Small-${VERSION}.zip $HERE
 echo TurtleShellZillions-${VERSION}.zip made
+echo TurtleShellZillions-Small-${VERSION}.zip made
 rm -fr $HOME/tmp/$$
