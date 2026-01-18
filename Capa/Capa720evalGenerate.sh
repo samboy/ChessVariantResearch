408b686a114be09c7a51e2a91f8b4ef2cdbdcc49
Sam Trenholme <strenholme.usenet@gmail.com>
2026-01-18 09:21:28 -0600

    Some refinements to the scripts which evaluate Capa720 positions

--- Capa/Capa720evalGenerate.sh
+++ Capa/Capa720evalGenerate.sh
@@ -3,7 +3,7 @@
 # This script evaluates all 720 Capa720 positions to see white’s 
 # centipawn advantage
 
-for setup in $( lunacy CapaSetups.lua  | awk -F/ '{print toupper($1)}' ) ; do
+for setup in $( lunacy Capa720Setups.lua | awk -F/ '{print toupper($1)}' ) ; do
   echo $setup
   lunacy SchoolMarshallEval.lua $setup | tail -1 >> Capa720eval.txt
 done
--- Capa/Capa720evalScore.sh
+++ Capa/Capa720evalScore.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
+FILENAME="$1"
+if [ -z "$FILENAME" ] ; then
+  FILENAME="Capa720evalNNUE21ply.txt"
+fi
+
 # Rank Capa720 setups, but ties all have same rank
 
-cat Capa720evalNNUE21ply.txt  | tr -d '\015' | awk '
+cat $FILENAME | tr -d '\015' | awk '
   {print $3 " " $0 " " ++a}' | sort -n | awk '
   {n++;score=$1;if(score != oldscore){show=n}print $0 " " show;oldscore=score}'
