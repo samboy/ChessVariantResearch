45bf92f89040f86271c92cf2cb58d9d4e144f3e9
Sam Trenholme <strenholme.usenet@gmail.com>
2026-01-19 19:30:37 -0600

    Begin work on Lua script to study the Capa openings

---
+++ Capa/Capa720Study.lua
@@ -0,0 +1,136 @@
+#!/bin/sh
+_rem=--[=[
+
+LUNACY=""
+if command -v lunacy64 >/dev/null 2>&1 ; then
+  LUNACY=lunacy64
+elif command -v lua5.1 >/dev/null 2>&1 ; then
+  LUNACY=lua5.1
+elif command -v lua-5.1 >/dev/null 2>&1 ; then
+  LUNACY=lua-5.1
+elif command -v lunacy >/dev/null 2>&1 ; then
+  LUNACY=lunacy
+elif command -v luajit >/dev/null 2>&1 ; then
+  LUNACY=luajit # I assume luajit will remain frozen at Lua 5.1
+fi
+if [ -z "$LUNACY" ] ; then
+  echo Please install Lunacy or Lua 5.1
+  echo https://github.com/samboy/lunacy
+  exit 1
+fi
+
+exec $LUNACY $0 "$@"
+
+# ]=]1
+-- This script is written in Lua 5.1
+
+-- This script has been donated to the public domain in 2026 by Sam Trenholme
+-- If, for some reason, a public domain declation is not acceptable, it
+-- may be licensed under the following terms:
+
+-- Copyright 2026 Sam Trenholme
+-- Permission to use, copy, modify, and/or distribute this software for
+-- any purpose with or without fee is hereby granted.
+-- THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL
+-- WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES
+-- OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+-- ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+-- WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+-- ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+-- OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+-- Create a new “board” (really, row) where we will place pieces
+function initBoard(size)
+  local out = {}
+  for a=1,size do
+    out[a] = " "
+  end
+  return out
+end
+
+-- Add a piece to the board, on the #place available empty square
+function addPiece(board, piece, place)
+  local count = 0
+  if type(board) ~= 'table' then return false end
+  if type(piece) ~= 'string' then piece = 'b' end 
+  if type(place) ~= 'number' then place = 1 end
+  for a=1,#board do
+    if board[a] == " " then
+      place = place - 1
+      if place < 1 then
+        board[a] = piece
+        return true
+      end
+    end
+  end
+  return false
+end
+
+-- Output a Capa720 setup, given a number from 0 to 719
+function Capa720(setup)
+  if type(setup) ~= 'number' then setup = 1 end
+  local set = setup
+  local board = initBoard(10)
+  board[1] = 'r'
+  board[10] = 'r'
+  board[6] = 'k'
+  -- In Chess960, we place the light bishop before the dark one
+  local bishop1 = set % 3
+  set = math.floor(set / 3)
+  bishop1 = bishop1 + 1
+  bishop1 = bishop1 * 2
+  if bishop1 == 6 then bishop1 = 8 end
+  board[bishop1] = 'b'
+  -- Dark bishop
+  local bishop2 = set % 4
+  set = math.floor(set / 4)     
+  bishop2 = bishop2 * 2
+  bishop2 = bishop2 + 3
+  board[bishop2] = 'b'
+  -- Queen
+  local queen = set % 5
+  queen = queen + 1
+  set = math.floor(set / 5)
+  addPiece(board,'q',queen)
+  -- Marshal/Chancellor
+  local chan = set % 4
+  chan = chan + 1
+  addPiece(board,'c',chan)
+  set = math.floor(set / 4)
+  -- Archbishop
+  local arch = set % 3
+  arch = arch + 1
+  addPiece(board,'a',arch)
+  -- Knights
+  addPiece(board,'n')
+  addPiece(board,'n')
+  -- Done
+  return board
+end  
+
+-- Given a board array, output the setup as ASCII 
+function board2ASCII(board)
+  local out = ""
+  for a=1,#board do
+    out = out .. tostring(board[a])
+  end
+  return out
+end
+
+-- Given a board array, output the setup as PGN
+function board2PGN(board)
+  local out = ""
+  local line = board2ASCII(board)
+  local pawns = ""
+  for a=1,#board do
+    pawns = pawns .. "p"
+  end
+  local empty = tostring(#board)
+  out = line .. "/" .. pawns .. "/" 
+  out = out .. empty .. "/" .. empty .. "/" .. empty .. "/" .. empty .. "/" 
+  out = out .. pawns:upper() .. "/" .. line:upper()
+  out = out .. "_w_KQkq_-_0_1"
+  return out
+end
+
+-- CODE HERE
