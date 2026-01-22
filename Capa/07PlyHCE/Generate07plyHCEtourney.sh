cf6b618fc7bc268b12dfa05f41ce1c1e603e206c
Sam Trenholme <strenholme.usenet@gmail.com>
2026-01-22 15:10:37 -0600

    Add tools to generate 7-ply HCE games to 07plyHCE directory

---
+++ Capa/07PlyHCE/Generate07plyHCEtourney.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+SETUP="$1"
+if [ -z "$SETUP" ] ; then
+  # Embassy chess is default
+  SETUP = RNBACKQBNR
+fi
+COUNT="$2"
+if [ -z "$COUNT " ] ; then
+  COUNT=100
+fi
+A=1 
+while [ $A -le $COUNT ] ; do 
+	echo $A; A=$(( $A + 1 )); 
+	lunacy FinessePlayXboard.lua $SETUP >> ${SETUP}-Chess07PlyHCE.txt; 
+	echo 'Game ended' >> ${SETUP}-Chess07PlyHCE.txt; sleep 3 
+done
+
+
