27e2251477601c336a073b902cd58151c97c8059
Sam Trenholme <strenholme.usenet@gmail.com>
2026-01-22 15:06:43 -0600

    Put 7-ply HCE games in their own folder

---
+++ Capa/07PlyHCE/XboardTally.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+# Tally xboard interface (HCE) games
+
+FILENAME="$1"
+if [ -z "$FILENAME" ] ; then
+  FILENAME=Finesse12PlyHCE.txt.xz
+fi
+COMMAND=grep
+if echo $FILENAME | grep -F .xz ; then
+  COMMAND=xzgrep
+fi
+COUNT="$2"
+if [ -n "$COUNT" ] ; then
+  $COMMAND '{' $FILENAME | head -$COUNT > foo-$$
+  FILENAME="foo-$$"
+fi
+
+$COMMAND '{' $FILENAME | cut -f2 -d{ | tr -d '\015' | tr -d } | awk '
+{a[$1]++;n++}END{for(b in a){print b " " a[b] " " a[b]/n*100 "%"}
+print n " games played";
+print ((a["White"] - a["Black"]) / n * 100) "% White winning edge"
+print ((a["White"] + (a["Draw"] / 2)) / n * 100) "% White score"
+}'
+
+rm -f foo-$$
