589885a5d8ae30c9ac0e0d0c4e7fa40897821a88
Sam Trenholme <strenholme.usenet@gmail.com>
2026-01-22 17:10:14 -0600

    Get tourneys to work in 07plyHCE directory

--- Capa/07PlyHCE/Generate07plyHCEtourney.sh
+++ Capa/07PlyHCE/Generate07plyHCEtourney.sh
@@ -3,12 +3,13 @@
 SETUP="$1"
 if [ -z "$SETUP" ] ; then
   # Embassy chess is default
-  SETUP = RNBACKQBNR
+  SETUP=RNBACKQBNR
 fi
 COUNT="$2"
-if [ -z "$COUNT " ] ; then
+if [ -z "$COUNT" ] ; then
   COUNT=100
 fi
+cp ../capablanca-bb644ef32758.nnue .
 A=1 
 while [ $A -le $COUNT ] ; do 
 	echo $A; A=$(( $A + 1 )); 
