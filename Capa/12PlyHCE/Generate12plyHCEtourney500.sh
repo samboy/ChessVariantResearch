567134c3ff4e82eb81a3bf76c57716061b19d988
Sam Trenholme <strenholme.usenet@gmail.com>
2026-01-23 11:17:30 -0600

    Add files needed to run 500 game 12-ply tourney

---
+++ Capa/12PlyHCE/Generate12plyHCEtourney500.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+SETUP="$1"
+if [ -z "$SETUP" ] ; then
+  # Embassy chess is default
+  SETUP=RNBACKQBNR
+fi
+COUNT="$2"
+if [ -z "$COUNT" ] ; then
+  COUNT=500
+fi
+cp ../capablanca-bb644ef32758.nnue .
+A=1 
+while [ $A -le $COUNT ] ; do 
+	echo $A; A=$(( $A + 1 )); 
+	lunacy Play12plyXboard.lua $SETUP >> 500/${SETUP}-Chess07PlyHCE.txt; 
+	echo 'Game ended' >> 500/${SETUP}-Chess07PlyHCE.txt; sleep 3 
+done
+
+
