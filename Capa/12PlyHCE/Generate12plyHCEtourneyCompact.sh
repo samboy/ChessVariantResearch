1dced5a83ac18298e7cb12ab8d6ab6fd1025c4e8
Sam Trenholme <strenholme.usenet@gmail.com>
2026-01-25 04:24:51 -0600

    Start making compact 500-game tourneys

---
+++ Capa/12PlyHCE/Generate12plyHCEtourneyCompact.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+N=1
+SETUP="$1"
+if [ -z "$SETUP" ] ; then
+  # Finesse chess is default
+  SETUP=RNABCKBQNR
+fi
+FILE=${SETUP}/${SETUP}-${N}-Chess12PlyHCE.txt
+while [ -e "$FILE" ] || [ -e "${FILE}.xz" ] ; do
+  N=$(( $N + 1 ))
+  FILE=${SETUP}/${SETUP}-${N}-Chess12PlyHCE.txt
+done
+echo $FILE
+COUNT="$2"
+if [ -z "$COUNT" ] ; then
+  COUNT=500
+fi
+cp ../capablanca-bb644ef32758.nnue .
+A=1 
+while [ $A -le $COUNT ] ; do 
+	echo $A; A=$(( $A + 1 )); 
+	echo 'Setup is ' $SETUP >> $FILE
+	lunacy Play12plyXboard.lua $SETUP | grep { >> $FILE 
+done
+
+
