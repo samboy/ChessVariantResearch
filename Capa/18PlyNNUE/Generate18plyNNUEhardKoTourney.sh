8e093645c1cd75029dca8eebb610b875962bed6a
Sam Trenholme <strenholme.usenet@gmail.com>
2026-01-26 19:06:39 -0600

    Add scripts for 18-ply Capablanca tourneys

---
+++ Capa/18PlyNNUE/Generate18plyNNUEhardKoTourney.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+N=1
+SETUP="$1"
+if [ -z "$SETUP" ] ; then
+  # Finesse chess is default
+  SETUP=RNABCKBQNR
+fi
+FILE=${SETUP}-hardKomi/${SETUP}-${N}-Chess12PlyNNUE.txt
+while [ -e "$FILE" ] || [ -e "${FILE}.xz" ] ; do
+  N=$(( $N + 1 ))
+  FILE=${SETUP}-hardKomi/${SETUP}-${N}-Chess12PlyNNUE.txt
+done
+echo $FILE
+COUNT="$2"
+if [ -z "$COUNT" ] ; then
+  COUNT=100
+fi
+cp ../capablanca-bb644ef32758.nnue .
+A=1 
+while [ $A -le $COUNT ] ; do 
+	echo $A; A=$(( $A + 1 )); 
+	lunacy Play18plyUCI-hardKo.lua $SETUP >> $FILE 
+	#lunacy Play18plyUCI-hardKo.lua $SETUP | grep { >> $FILE 
+	sleep 3
+done
+
+
