2feb6e72c945a4ad7c60520e833fbaade5616d88
Sam Trenholme <strenholme.usenet@gmail.com>
2026-01-29 07:32:16 -0600

    Be ready to run Classic chess tourney

---
+++ Capa/18PlyProperMateBugFix/Generate18plyNNUEClassicChess.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+N=1
+SETUP="$1"
+if [ -z "$SETUP" ] ; then
+  # Classic chess is default
+  SETUP=RNBQKBNR
+fi
+FILE=${SETUP}-noKo/${SETUP}-${N}-Chess12PlyNNUE.txt
+while [ -e "$FILE" ] || [ -e "${FILE}.xz" ] ; do
+  N=$(( $N + 1 ))
+  FILE=${SETUP}-noKo/${SETUP}-${N}-Chess12PlyNNUE.txt
+done
+echo $FILE
+COUNT="$2"
+if [ -z "$COUNT" ] ; then
+  COUNT=100
+fi
+cp ../capablanca-bb644ef32758.nnue .
+A=1 
+while [ $A -le $COUNT ] ; do 
+	echo $A; A=$(( $A + 1 )); 
+	lunacy PlayClassicChessUCI-noKo.lua $SETUP >> $FILE 
+	#lunacy PlayClassicChessUCI-noKo.lua $SETUP | grep { >> $FILE 
+	sleep 3
+done
+
+
