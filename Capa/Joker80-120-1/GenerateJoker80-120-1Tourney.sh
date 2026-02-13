937f90477226bcf7a21813a02518d7d2e927dd76
Sam Trenholme <strenholme.usenet@gmail.com>
2026-02-12 19:03:21 -0600

    Add code to have Joker80 120/1 Finesse Chess tourney

---
+++ Capa/Joker80-120-1/GenerateJoker80-120-1Tourney.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+N=1
+SETUP="$1"
+if [ -z "$SETUP" ] ; then
+  # Finesse chess is default
+  SETUP=RNABCKBQNR
+fi
+FILE=${SETUP}-120-1/${SETUP}-${N}-Chess18PlyNNUE.txt
+while [ -e "$FILE" ] || [ -e "${FILE}.xz" ] ; do
+  N=$(( $N + 1 ))
+  FILE=${SETUP}-120-1/${SETUP}-${N}-Chess18PlyNNUE.txt
+done
+echo $FILE
+COUNT="$2"
+if [ -z "$COUNT" ] ; then
+  COUNT=500
+fi
+A=1 
+while [ $A -le $COUNT ] ; do 
+	echo $A; A=$(( $A + 1 )); 
+	lunacy FinessePlayJoker80.lua $SETUP 120 1 >> $FILE 
+	sleep 3
+done
+
