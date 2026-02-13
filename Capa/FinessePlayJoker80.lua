88666a9fe49d66a06cd4b7be7f7b64476c55a2ca
Sam Trenholme <strenholme.usenet@gmail.com>
2026-02-12 18:51:56 -0600

    Add interface for Joker 80
    Usage:
    
    lunacy FinessePlayJoker80.lua RNABCKBQNR 120 1
    
    This will have Joker80 play Finesse with 120 moves per minute

---
+++ Capa/FinessePlayJoker80.lua
@@ -0,0 +1,174 @@
+-- Placed in the public domain 2021,2026 by Sam Trenholme
+-- This is a lunacy (Lua + Steve Donovan's spawner lib) implementation of
+-- a simple Chess/Chess variants client
+--
+-- This client is a “randomized” version of Fairy-Stockfish:  It looks
+-- at the top MultiPV number of moves (default: 3), and chooses one within
+-- 50 centipawns of what it thinks is the best move at random.
+--
+-- This client requires the Fairy-Stockfish program to be installed
+-- and available with the name fairy-stockfish-largeboard_x86-64
+-- (if it has another name, change "ChessEngine" below)
+
+-- Let's look at win/lose/draw ratio for different capa setups
+vSetup = "RNABCKBQNR"
+if #arg >= 1 then
+  vSetup = arg[1]
+end
+NMoves = 60 
+if #arg >= 2 then
+  NMoves = tonumber(arg[2])
+  if NMoves < 1 then NMoves = 1 end
+end
+minutesPerNMoves = 1
+if #arg >= 3 then
+  minutesPerNMoves = tonumber(arg[3])
+  if minutesPerNMoves < 1 then minutesPerNMoves = 1 end
+end
+
+-- params is a table with the "user tunable" parameters
+params = {
+  -- See https://github.com/ianfab/Fairy-Stockfish for the Chess engine
+  -- This is the name of the chess engine, as it appears in one's $PATH
+  ChessEngine = "joker80",
+  -- This is the number of lines we look at and consider for our next move
+  MultiPV = 1,
+  -- The name of the variant we will look at.  This needs to be a variant
+  -- Fairy-Stockfish supports
+  variantName = "capablanca",
+  -- variantName = "chess",
+  -- The opening setup (or position) we will play from in the game
+  -- Note: This currently only works with 8xN games (8x8, 8x10, 8x12, etc.)
+  variantSetup = vSetup,
+  -- It's also possible to set up any arbitrary FEN, not just a mirrored
+  -- 8x# backrank opening setup
+  -- This is an argument given to the Fairy-Stockfish "setboard" command
+  -- variantFEN = "rnabckbqnr/pppppppppp/10/10/10/10/PPPPPPPPPP/RNABCKBQNR " ..
+  --          "w KQkq - 0 1",
+  -- variantFEN = false, -- Use default opening setup for variant
+  -- After this many plies are searched, decide on a move to make
+  searchPly = 7,
+  -- Opening to play.  Format is like this: "f2f4 f7f5", where each move has
+  -- four letters (from, to) or five letters (for pawn promotions: b7b8q)
+  -- King move for castling (e.g. e1g1 with normal RNBQKBNR chess).  Spaces
+  -- between openings
+  opening = false,
+}
+
+-- Here be dragons below
+ChessEngine = params["ChessEngine"]
+MultiPV = tonumber(params["MultiPV"])
+variantName = params["variantName"]
+if type(params["variantFEN"]) == "string" then
+  variantFEN = params["variantFEN"]
+else
+  variantFEN = false
+end
+
+-- If they specify a variantSetup (e.g. RNBQKBNR or RANBQKBNCR), convert
+-- that in to the appropriate 8x# FEN string
+if type(params["variantSetup"]) == "string" then
+  local setup = params["variantSetup"]
+  -- Black's pieces
+  variantFEN = setup:lower() .. "/"
+  -- Black's pawns
+  for a = 1,#setup do
+    variantFEN = variantFEN .. "p"
+  end
+  -- The 4-row blank area between setups
+  -- This code currently only works with 8x# setups
+  for a = 1, 4 do
+    variantFEN = variantFEN .. "/" .. tostring(#setup)
+  end
+  variantFEN = variantFEN .. "/"
+  -- White's pawns
+  for a = 1,#setup do
+    variantFEN = variantFEN .. "P"
+  end
+  variantFEN = variantFEN .. "/" .. setup:upper() .. " w KQkq - 0 1"
+end
+
+-- How many ply do we look ahead per move
+searchPly = tonumber(params["searchPly"])
+
+-- Sanity for numeric vaues
+if not MultiPV or MultiPV < 2 then
+  print("MultiPV too small/not set, using 1")
+  MultiPV = 1
+end
+if not searchPly or searchPly < 7 then 
+  print("searchPly too small/not set, using 21") 
+  searchPly = 7
+end
+
+----------------------- rStrSplit() -----------------------
+-- This does a simple split for a given string, useful for simple CSV
+-- Input: string (single CSV line), split character
+-- Output: An array with each field in the CSV line
+function rStrSplit(s, splitOn)
+  if not splitOn then splitOn = "," end
+  local place = 1
+  local out = {}
+  local last = 1
+  while place do
+    place = string.find(s, splitOn, place, true)
+    if place then
+      out[#out + 1] = string.sub(s, last, place - 1)
+      place = place + 1
+      last = place
+    end
+  end
+  out[#out + 1] = string.sub(s, last, -1)
+  return out
+end
+
+openmove = {}
+thismove = 0
+if type(params["opening"]) == "string" then
+  thismove = 1
+  openmove = rStrSplit(params["opening"]," ")
+end 
+
+if spawner == nil then
+  print("I need Steve Donovan's spawner lib to continue!")
+  print("Download Lunacy at https://github.com/samboy/lunacy")
+  os.exit(1)
+end
+
+w,r = spawner.popen2(ChessEngine)
+-- w:write("setoption name MultiPV value " .. tostring(MultiPV) .. "\n")
+-- Load NNUE
+-- w:write("setoption name EvalFile value capablanca-bb644ef32758.nnue\n")
+w:write("xboard\n")
+w:write("variant " .. variantName .. "\n")
+if variantFEN then
+  w:write("setboard " .. variantFEN .. "\n")
+end
+-- w:write("sd 7\n") -- Depth 7 moves
+w:write("level " .. NMoves .. " " .. minutesPerNMoves .. " 0\n") -- Set time
+w:write("p\n") -- Joker80 uses “p”, not “d” like (fairy-)stockfish
+w:flush()
+lineFromEngine = ""
+while not string.match(lineFromEngine,'GameEval') do -- Different p output
+  lineFromEngine = r:read()
+  print(lineFromEngine)
+end
+io.flush()
+
+w:write("go\n")
+w:flush()
+while true do
+  lineFromEngine = r:read()
+  if not lineFromEngine then
+    os.exit(0)
+  end
+  if lineFromEngine:match('%{') then
+    print(lineFromEngine)
+    os.exit(0)
+  end
+  print(lineFromEngine)
+  if lineFromEngine:match('^move') then
+    w:write("go\n")
+    w:flush()
+  end
+end
