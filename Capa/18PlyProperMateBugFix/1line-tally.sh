dee6834f8447bc51686ab52a2e526f7a6c4bb5a2
Sam Trenholme <strenholme.usenet@gmail.com>
2026-02-17 21:35:49 -0600

    Update 1-line tally
    Here are the Finesse Chess results:
    
    100 games - 49/19/32 60.4938% White decisive wins
    100 games - 37/27/36 50.6849% White decisive wins
    100 games - 38/13/49 43.6782% White decisive wins
    100 games - 45/16/39 53.5714% White decisive wins
    100 games - 47/17/36 56.6265% White decisive wins
    100 games - 51/17/32 61.4458% White decisive wins
    100 games - 46/20/34 57.5% White decisive wins
    100 games - 45/29/26 63.3803% White decisive wins
    100 games - 45/21/34 56.962% White decisive wins
    100 games - 46/19/35 56.7901% White decisive wins
    
    For all 1000 games:
    
    1000 games - 44.9/19.8/35.3 55.985% White decisive wins
    
    With Classic Chess:
    
    100 games - 26/54/20 56.5217% White decisive wins
    100 games - 33/45/22 60% White decisive wins
    100 games - 25/54/21 54.3478% White decisive wins
    100 games - 30/53/17 63.8298% White decisive wins
    100 games - 28/52/20 58.3333% White decisive wins
    
    And for all 500 games there:
    
    500 games - 28.4/51.6/20 58.6777% White decisive wins

--- Capa/18PlyProperMateBugFix/1line-tally.sh
+++ Capa/18PlyProperMateBugFix/1line-tally.sh
@@ -18,14 +18,16 @@ fi
 
 LINE=$( $COMMAND '{' $FILENAME | cut -f2 -d{ | tr -d '\015' | tr -d } | awk '
 {a[$1]++;n++}END{
-print a["White"] / n * 100 "/" a["Black"] / n * 100 "/" a["draw"] / n * 100
+print n " games - "
+print a["White"] / n * 100 "/" a["draw"] / n * 100 "/" a["Black"] / n * 100
 #print ((a["White"] - a["Black"]) / n * 100) "% White edge - "
 #print ((a["White"] + (a["Draw"] / 2)) / n * 100) "% Score - "
 #print a["Draw"] / n * 100 "% Draw"
 print (a["White"] / (a["White"] + a["Black"]) * 100) "% White decisive wins"
 }' )
 
-SETUP=$( echo $FILENAME | cut -f1 -d- )
-echo $SETUP $LINE
+#SETUP=$( echo $FILENAME | cut -f1 -d- )
+#echo $SETUP $LINE
+echo $LINE
 
 rm -f foo-$$
