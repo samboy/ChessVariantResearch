29eba48139a0f6bf7b3919349034467d1655b025
Sam Trenholme <strenholme.usenet@gmail.com>
2026-02-17 21:13:07 -0600

    Add 1-line tally to show how much these numbers vary
    The first 10 lines are for 50 or 100 games (mostly 100)
    RNABCKBQNR 49/32/19 60.4938% White decisive wins
    RNABCKBQNR 37/36/27 50.6849% White decisive wins
    RNABCKBQNR 38/49/13 43.6782% White decisive wins
    RNABCKBQNR 45/39/16 53.5714% White decisive wins
    RNABCKBQNR 50/34/16 59.5238% White decisive wins
    RNABCKBQNR 44/38/18 53.6585% White decisive wins
    RNABCKBQNR 51/32/17 61.4458% White decisive wins
    RNABCKBQNR 46/34/20 57.5% White decisive wins
    RNABCKBQNR 45/26/29 63.3803% White decisive wins
    RNABCKBQNR 45/34/21 56.962% White decisive wins
    RNABCKBQNR 46/35/19 56.7901% White decisive wins
    
    And, in total, for all 1000 games:
    
    1000 games 44.9/35.3/19.8 55.985% White decisive wins

---
+++ Capa/18PlyProperMateBugFix/1line-tally.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+# Tally xboard interface (HCE) games
+
+FILENAME="$1"
+if [ -z "$FILENAME" ] ; then
+  FILENAME=Finesse12PlyHCE.txt.xz
+fi
+COMMAND=grep
+if echo $FILENAME | grep -F .xz > /dev/null ; then
+  COMMAND=xzgrep
+fi
+COUNT="$2"
+if [ -n "$COUNT" ] ; then
+  $COMMAND '{' $FILENAME | head -$COUNT > foo-$$
+  FILENAME="foo-$$"
+fi
+
+LINE=$( $COMMAND '{' $FILENAME | cut -f2 -d{ | tr -d '\015' | tr -d } | awk '
+{a[$1]++;n++}END{
+print a["White"] / n * 100 "/" a["Black"] / n * 100 "/" a["draw"] / n * 100
+#print ((a["White"] - a["Black"]) / n * 100) "% White edge - "
+#print ((a["White"] + (a["Draw"] / 2)) / n * 100) "% Score - "
+#print a["Draw"] / n * 100 "% Draw"
+print (a["White"] / (a["White"] + a["Black"]) * 100) "% White decisive wins"
+}' )
+
+SETUP=$( echo $FILENAME | cut -f1 -d- )
+echo $SETUP $LINE
+
+rm -f foo-$$
