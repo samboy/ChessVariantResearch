066ec5d472a6b5a9a12bcfe6eae8dca4ea77d1d2
Sam Trenholme <strenholme.usenet@gmail.com>
2026-02-21 06:45:14 -0600

    Sicilian Najdorf: 300 games
    White 96 32%
    Draw 139 46.3333%
    Black 65 21.6667%
    300 games played
    10.3333% White winning edge
    55.1667% White score
    59.6273% White decisive wins

---
+++ Chess960/RNBQKBNR-byOpening/TourneyNajdorf.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+N=1
+OPENING="Najdorf"
+FILE=${OPENING}/${OPENING}-${N}-Chess18PlyNNUE.txt
+while [ -e "$FILE" ] || [ -e "${FILE}.xz" ] ; do
+  N=$(( $N + 1 ))
+  FILE=${OPENING}/${OPENING}-${N}-Chess18PlyNNUE.txt
+done
+echo $FILE
+COUNT="$2"
+if [ -z "$COUNT" ] ; then
+  COUNT=100
+fi
+A=1 
+while [ $A -le $COUNT ] ; do 
+	echo $A; A=$(( $A + 1 )); 
+	lunacy PlayClassicChess-opening.lua 'e2e4 c7c5 g1f3 d7d6 d2d4 c5d4 f3d4 g8f6 b1c3 a7a6' >> $FILE
+	sleep 3
+done
+
+
