ff88e2517c323f27b5fd1233b2d224bd25abca09
Sam Trenholme <strenholme.usenet@gmail.com>
2026-02-25 15:41:27 -0600

    Added tool to find good Double Fischer Random setups
    This setup is perfectly balanced for White and Black at 21-ply
    Stockfish evaluation:
    
    nbrqbkrn/pppppppp/8/8/8/8/PPPPPPPP/QNRBBKNR w KQkq - 0
    
    https://lichess.org/analysis/chess960?fen=nbrqbkrn/pppppppp/8/8/8/8/PPPPPPPP/QNRBBKNR_w_KQkq_-_0

---
+++ Chess921600/FindBalancedSetup.lua
@@ -0,0 +1,281 @@
+#!/bin/sh
+_rem=--[=[
+
+LUNACY=""
+if command -v lunacy64 >/dev/null 2>&1 ; then
+  LUNACY=lunacy64
+#elif command -v lua5.1 >/dev/null 2>&1 ; then
+#  LUNACY=lua5.1
+#elif command -v lua-5.1 >/dev/null 2>&1 ; then
+#  LUNACY=lua-5.1
+elif command -v lunacy >/dev/null 2>&1 ; then
+  LUNACY=lunacy
+#elif command -v luajit >/dev/null 2>&1 ; then
+#  LUNACY=luajit # I assume luajit will remain frozen at Lua 5.1
+fi
+if [ -z "$LUNACY" ] ; then
+  echo Please install Lunacy
+  echo https://github.com/samboy/lunacy
+  exit 1
+fi
+
+exec $LUNACY $0 "$@"
+
+# ]=]1
+-- Placed in the public domain 2021,2026 by Sam Trenholme
+-- This is a lunacy (Lua + Steve Donovan's spawner lib) implementation of
+-- a simple Chess/Chess variants client
+--
+-- This client is a “randomized” version of Fairy-Stockfish:  It looks
+-- at the top MultiPV number of moves (default: 3), and chooses one within
+-- 50 centipawns of what it thinks is the best move at random.
+--
+-- This client requires the Stockfish program to be installed
+
+if #arg >= 1 then
+  a1 = arg[1]
+else
+  a1 = ""
+end
+if a1:match("%?") or a1:match("%-") or a1:match("[Hh]") then 
+  print(
+     "Usage: lunacy FindBalancedSetup.lua {iterations} {seed}")
+  print(
+     "Example: lunacy FindBalancedSetup.lua 100 SomeRandomText")
+  os.exit(0)
+end
+
+iterations = 10
+if #arg >= 1 then
+  iterations = tonumber(a1)
+else 
+  iterations = 10
+end
+if not iterations or iterations < 1 then
+  iterations = 10
+end
+
+local thisseed = os.time()
+if #arg >= 2 then
+  thisseed = arg[2]
+end
+
+-- Here be dragons below
+if rg32 == nil then
+  print("I need the rg32 lib to continue!")
+  print("Download Lunacy at https://github.com/samboy/lunacy")
+  os.exit(1)
+end
+rg32.randomseed(thisseed)
+
+searchPly = 21
+if not searchPly or searchPly < 7 then 
+  print("searchPly too small/not set, using 21") 
+  searchPly = 21
+end
+
+------------------------------------------------------------------
+-- Functions to make a Chess960 board
+-- Create a new “board” (really, row) where we will place pieces
+function initBoard(size)
+  local out = {}
+  for a=1,size do
+    out[a] = " "
+  end
+  return out
+end
+
+-- Add a piece to the board, on the #place available empty square
+function addPiece(board, piece, place)
+  local count = 0
+  if type(board) ~= 'table' then return false end
+  if type(piece) ~= 'string' then piece = 'b' end 
+  if type(place) ~= 'number' then place = 1 end
+  for a=1,#board do
+    if board[a] == " " then
+      place = place - 1
+      if place < 1 then
+        board[a] = piece
+        return true
+      end
+    end
+  end
+  return false
+end
+
+-- Output a Chess960 setup, given a number from 0 to 959
+function Chess960(setup)
+  if type(setup) ~= 'number' then setup = 1 end
+  local set = setup
+  local board = initBoard(8)
+  local bishop1 = set % 4
+  set = math.floor(set / 4)
+  bishop1 = bishop1 + 1
+  bishop1 = bishop1 * 2
+  board[bishop1] = 'b'
+  local bishop2 = set % 4
+  set = math.floor(set / 4)     
+  bishop2 = bishop2 * 2
+  bishop2 = bishop2 + 1
+  board[bishop2] = 'b'
+  local queen = set % 6
+  queen = queen + 1
+  set = math.floor(set / 6)
+  addPiece(board,'q',queen)
+  -- Knights are tricky
+  local knights = set % 10
+  knights = knights + 1
+  -- This could be calculated, but since we only have to do this with
+  -- knights, just use a two dimensional array
+  local knightArray = {{1,1}, {1,2}, {1,3}, {1,4}, {2,2}, {2,3}, {2,4},
+                       {3,3}, {3,4}, {4,4}}
+  addPiece(board,'n',knightArray[knights][1])
+  addPiece(board,'n',knightArray[knights][2])
+  -- Once Bishops, Queen, Knights are placed, king is always between
+  -- the two rooks so there is only one possible setup at this point
+  addPiece(board,'r')
+  addPiece(board,'k')
+  addPiece(board,'r')
+  return board
+end
+
+-- Given a board array, output the setup as ASCII 
+function board2ASCII(board)
+  local out = ""
+  for a=1,#board do
+    out = out .. tostring(board[a])
+  end
+  return out
+end
+
+-- Given 2 board arrays, output the setup as PGN
+function boards2PGN(board1, board2)
+  local out = ""
+  local topline = board2ASCII(board1):lower()
+  local bottomline = board2ASCII(board2):upper()
+  local pawns = ""
+  for a=1,#board1 do
+    pawns = pawns .. "p"
+  end
+  local empty = tostring(#board1)
+  out = topline .. "/" .. pawns .. "/" 
+  out = out .. empty .. "/" .. empty .. "/" .. empty .. "/" .. empty .. "/" 
+  out = out .. pawns:upper() .. "/" .. bottomline
+  out = out .. " w KQkq - 0 1"
+  return out
+end
+
+-- Return true is the board is a Chess18 position (rooks and king unmoved
+-- from classic Chess)
+function isChess18(board) 
+  if type(board) ~= 'table' then return nil end
+  if #board ~= 8 then return nil end
+  if board[1] == 'r' and board[5] == 'k' and board[8] == 'r' then
+    return true
+  end
+  return false
+end
+
+-- Get a random number from 0 to 959
+function randomSetupNumber() 
+  local number = 65280
+  local stop = 1
+  while number >= 65280 do
+    number = rg32.rand16()
+    stop = stop + 1
+    if stop > 1000 then
+      print("Infinite loop panic\n") os.exit(1)
+    end
+  end
+  number = number % 960
+  return number
+end
+
+function MakeFEN()
+  local White = Chess960(randomSetupNumber())
+  local Black = Chess960(randomSetupNumber())
+  local FEN = boards2PGN(Black, White)
+  if not FEN then return nil end
+  return FEN
+end
+
+----------------------- rStrSplit() -----------------------
+-- This does a simple split for a given string, useful for simple CSV
+-- Input: string (single CSV line), split character
+-- Output: An array with each field in the CSV line
+function rStrSplit(s, splitOn)
+  if not splitOn then splitOn = "," end
+  local place = 1
+  local out = {}
+  local last = 1
+  while place do
+    place = string.find(s, splitOn, place, true)
+    if place then
+      out[#out + 1] = string.sub(s, last, place - 1)
+      place = place + 1
+      last = place
+    end
+  end
+  out[#out + 1] = string.sub(s, last, -1)
+  return out
+end
+
+if spawner == nil then
+  print("I need Steve Donovan's spawner lib to continue!")
+  print("Download Lunacy at https://github.com/samboy/lunacy")
+  os.exit(1)
+end
+
+thisIteration = 1
+ChessEngine = "stockfish"
+w,r = spawner.popen2(ChessEngine)
+w:write("setoption name Use NNUE value true\n")
+w:write("setoption name UCI_Chess960 value true\n")
+w:write("setoption name MultiPV value 1\n")
+w:write("ucinewgame\n")
+minEval = 1000000
+for z=1,iterations do
+  thisFEN = MakeFEN()
+  w:write("position fen " .. thisFEN .. "\n")
+  w:write("d\n")
+  w:write("go depth 21\n")
+  w:flush()
+  lineFromEngine = ""
+  eval = -1
+  lines = {}
+  while not string.match(lineFromEngine,'^bestmove') do
+    local x = nil
+    local line = nil
+    lineFromEngine = r:read()
+    line, x = lineFromEngine:match("multipv (%d+) score cp ([%d%-]+)")
+    if x and tonumber(line) == 1 then
+      eval = x
+    end
+    local move = lineFromEngine:match(" pv (%S+) ")
+    if line then
+      lines[tonumber(line)] = {}
+      lines[tonumber(line)]['eval'] = tonumber(x) 
+      lines[tonumber(line)]['move'] = move
+    end
+    -- print(lineFromEngine) -- Do this if verbose
+    io.flush()
+  end
+  thisEval = math.abs(tonumber(lines[1]['eval']))
+  if thisEval < minEval then
+    minEval=thisEval
+    out = tostring(thisseed) .. "," .. thisIteration .. "," .. thisFEN .. ":" 
+    for a=1,#lines do
+      if lines[a]['move'] then
+        out = out .. lines[a]['eval'] .. ',' .. lines[a]['move'] .. ';'
+      end
+    end
+    print(out)
+  end
+  thisIteration = thisIteration + 1
+  io.flush()
+  if thisEval == 0 then
+    print("Eval 0 found, job done\n")
+    os.exit(0)
+  end
+end
+
---
+++ Chess921600/README.md
@@ -0,0 +1,54 @@
+# What this is
+
+There is a concern that 960 possible setups may not be enough, and some
+really smart grandmaster may have the ability to remember openings for
+all 960 setups.  While that seems far fetched, if it ever were to become
+a problem, we can have “Double Fischer Random” which has different 
+Chess960 setups for White and Black.
+
+There are 921,600 possible “Double Fischer Random” setups, hence the
+name Chess921600.
+
+However, then the concern is: Does a setup unfavorably favor Black or
+White?  And the answer is, with most setups, less so than with 
+Classic Chess (which gives White a 39 centipawn advantage).
+
+## This program
+
+This program looks at a Double Fischer Random setup, evaluates it with
+Stockfish (21-ply) and tells us how balanced it is.
+
+If the evaluation is 0, that means the setup is perfectly evaluated.
+
+For example, the seed 1772053877 gives us a 0 eval right off the bat:
+
+```
+$ lunacy FindBalancedSetup.lua 100 1772053877
+1772053877,1,nbrqbkrn/pppppppp/8/8/8/8/PPPPPPPP/QNRBBKNR w KQkq - 0 1:0,c2c4;
+Eval 0 found, job done
+```
+
+To use FindBalancedSetup.lua, the arguments are (both optional)
+
+* Iterations: How many random setups do we choose and then have Stockfish
+  evaluate at 21-ply depth?
+* Seed: The seed to make the setups (one seed can make unlimited setups,
+  the seed can be a number or a String)
+
+If we find a setup with an eval of 0, we stop our search.  Otherwise, 
+we list setups we find which have a lower White or Black advantage than 
+the last setup listed:
+
+```
+$ lunacy FindBalancedSetup.lua 100 777
+777,1,bbnqrknr/pppppppp/8/8/8/8/PPPPPPPP/NRNQKBBR w KQkq - 0 1:-116,d2d4;
+777,2,rnknbqrb/pppppppp/8/8/8/8/PPPPPPPP/RBNQBKNR w KQkq - 0 1:45,c2c3;
+777,4,bbqrknrn/pppppppp/8/8/8/8/PPPPPPPP/RKBBNRNQ w KQkq - 0 1:11,a2a4;
+777,37,bbrnkrnq/pppppppp/8/8/8/8/PPPPPPPP/QNNRBKRB w KQkq - 0 1:-3,g2g3;
+```
+
+Here, the first setup we found really favored Black, the second setup
+we found really favored White, and the fourth setup has a much smaller
+White advantage than Classic Chess does.  Finally, on the 37th setup, we
+found a setup which only slightly favored Black.
+
