811c59c0352c9e8f646f2f7629d610ba343e57e6
Sam Trenholme <strenholme.usenet@gmail.com>
2026-03-01 15:33:57 -0600

    turtleShellGrid.lua: Add terms if they don’t want public domain

--- TurtleShell/turtleShellGrid.lua
+++ TurtleShell/turtleShellGrid.lua
@@ -32,6 +32,19 @@ exec $LUNACY $0 "$@"
 # ]=]1
 
 -- Donated to the public domain by Sam Trenholme 2021, 2022, 2026
+-- If, for some reason, a public domain declation is not acceptable, it
+-- may be licensed under the following terms:
+
+-- Copyright 2021-2026 Sam Trenholme
+-- Permission to use, copy, modify, and/or distribute this software for
+-- any purpose with or without fee is hereby granted.
+-- THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL
+-- WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES
+-- OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+-- ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+-- WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+-- ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+-- OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
 -- Make a "turtle shell" grid with a defined size in SVG format
 
