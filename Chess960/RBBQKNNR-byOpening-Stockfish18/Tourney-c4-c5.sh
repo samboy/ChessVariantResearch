d3e6854bc017b2c79a59c5a9fd5205ed48447c2e
Sam Trenholme <strenholme.usenet@gmail.com>
2026-03-13 12:10:21 -0600

    Add script for RBBQKNNR 1. c4 c5 tourney (Chess960/Chess18)

---
+++ Chess960/RBBQKNNR-byOpening-Stockfish18/Tourney-c4-c5.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+N=1
+OPENING="c4-c5"
+FILE=${OPENING}/${OPENING}-${N}-Chess18PlyNNUE.txt
+while [ -e "$FILE" ] || [ -e "${FILE}.xz" ] ; do
+  N=$(( $N + 1 ))
+  FILE=${OPENING}/${OPENING}-${N}-Chess18PlyNNUE.txt
+done
+echo $FILE
+COUNT="$2"
+if [ -z "$COUNT" ] ; then
+  COUNT=100
+fi
+A=1 
+while [ $A -le $COUNT ] ; do 
+	echo $A; A=$(( $A + 1 )); 
+	lunacy PlayChess18-opening.lua 'c2c4 c7c5' 18 RBBQKNNR >> $FILE 
+	sleep 3
+done
+
+
