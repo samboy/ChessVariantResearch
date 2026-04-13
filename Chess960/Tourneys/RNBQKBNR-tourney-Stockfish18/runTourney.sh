72424fa95a14469fb47bbf4e880f2ddb21e6ba9d
Sam Trenholme <strenholme.usenet@gmail.com>
2026-04-13 11:42:43 -0600

    Add files to run Classic Chess 7-15 ply tourney

---
+++ Chess960/Tourneys/RNBQKBNR-tourney-Stockfish18/runTourney.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+GROUP=02
+#SETUP=RNABCKBQNR
+SETUP=RNBQKBNR
+
+for PLY in 07 08 09 10 11 12 13 14 15 ; do 
+	lunacy64 ../../../EvalOrPlay.lua \
+		--play ${SETUP} \
+		$PLY 5 > ${SETUP}-${GROUP}-${PLY}ply.txt
+done
