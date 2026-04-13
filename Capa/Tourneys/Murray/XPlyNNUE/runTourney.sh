aa43383bbf7ccca24713ff32f81128c3ebe28c7f
Sam Trenholme <strenholme.usenet@gmail.com>
2026-04-13 17:33:59 -0600

    runTourney: Fix path to Lunacy script

--- Capa/Tourneys/Murray/XPlyNNUE/runTourney.sh
+++ Capa/Tourneys/Murray/XPlyNNUE/runTourney.sh
@@ -4,7 +4,7 @@ GROUP=01
 SETUP=RCNBQKBNAR
 
 for PLY in 07 08 09 10 11 12 13 14 15 ; do 
-	lunacy64 ../../../EvalOrPlay.lua \
+	lunacy64 ../../../../EvalOrPlay.lua \
 		--play ${SETUP} \
 		$PLY 5 > ${SETUP}-${GROUP}-${PLY}ply.txt
 done
