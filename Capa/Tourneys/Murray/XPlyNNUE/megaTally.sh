c92293518709bb7d5d51f5174023b43b04a01be6
Sam Trenholme <strenholme.usenet@gmail.com>
2026-04-13 19:17:48 -0600

    Add 10-ply and 11-ply Murray results
    07 100 games - 27/2/71  27.551% White decisive wins
    08 100 games - 47/8/45  51.087% White decisive wins
    09 100 games - 50/5/45  52.6316% White decisive wins
    10 100 games - 48/13/39 55.1724% White decisive wins
    11 100 games - 44/14/42 51.1628% White decisive wins
    
    Finesse:
    07 100 games - 54/2/44  55.102% White decisive wins
    08 100 games - 48/4/48  50% White decisive wins
    09 100 games - 54/6/40  57.4468% White decisive wins
    10 100 games - 50/8/42  54.3478% White decisive wins
    11 100 games - 44/10/46 48.8889% White decisive wins
    
    Classic:
    07 100 games - 40/33/27 59.7015% White decisive wins
    08 100 games - 41/41/18 69.4915% White decisive wins
    09 100 games - 45/33/22 67.1642% White decisive wins
    10 100 games - 35/43/22 61.4035% White decisive wins
    11 100 games - 46/41/13 77.9661% White decisive wins
    
    So, roughly:
    
    * Murray-Carrera is slightly more drawish than Finesse Chess
    * Murray-Carrera ever so slightly favors White compared to Finesse
    * Both favor White a lot less and have fewer draws than Classic Chess

--- Capa/Tourneys/Murray/XPlyNNUE/megaTally.sh
+++ Capa/Tourneys/Murray/XPlyNNUE/megaTally.sh
@@ -4,5 +4,5 @@
 
 for a in 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 ; do 
 	echo -n ${a}' ' 
-	./1line-tally.sh RNABCKBQNR-01-${a}ply.txt.xz 
+	./1line-tally.sh RCNBQKBNAR-01-${a}ply.txt.xz 
 done
