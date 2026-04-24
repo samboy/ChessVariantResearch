d97f28d6c950023409b8290de36faf6bbc909c4c
Sam Trenholme <strenholme.usenet@gmail.com>
2026-04-24 15:19:22 -0600

    XplyNNUE: Make tally.sh busybox compatible

--- Capa/Tourneys/Finesse/XPlyNNUE/tally.sh
+++ Capa/Tourneys/Finesse/XPlyNNUE/tally.sh
@@ -6,9 +6,9 @@ FILENAME="$1"
 if [ -z "$FILENAME" ] ; then
   FILENAME=Finesse12PlyHCE.txt.xz
 fi
-COMMAND=grep
+COMMAND=cat
 if echo $FILENAME | grep -F .xz ; then
-  COMMAND=xzgrep
+  COMMAND=xzcat
 fi
 COUNT="$2"
 if [ -n "$COUNT" ] ; then
@@ -16,7 +16,7 @@ if [ -n "$COUNT" ] ; then
   FILENAME="foo-$$"
 fi
 
-$COMMAND '{' $FILENAME | cut -f2 -d{ | tr -d '\015' | tr -d } | awk '
+$COMMAND $FILENAME | fgrep '{' | cut -f2 -d{ | tr -d '\015' | tr -d } | awk '
 {f=$1;if(f == "draw"){f="Draw"}a[f]++;n++}
 END{
   for(b in a){print b " " a[b] " " a[b]/n*100 "%"}
