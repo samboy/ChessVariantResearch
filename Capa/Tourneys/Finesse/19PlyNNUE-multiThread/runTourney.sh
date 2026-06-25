fb05e737b26232280782c5520853e5d34c992bb1
Sam Trenholme <git@samiam.org>
2026-06-24 20:26:31 -0600

    Adding script to run multi-thread tourney

---
+++ Capa/Tourneys/Finesse/19PlyNNUE-multiThread/runTourney.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+# Make sure capablanca-bb644ef32758.nnue is here
+THREAD="$1"
+if [ -z "$THREAD" ] ; then
+  echo 'Usage: $0 {thread}'
+  echo '{thread} is a three digit alphanumeric unique thread number'
+  exit 1
+exit
+
+COUNTER=1
+GROUP=$( echo $COUNTER | awk '{print sprintf("%03d",$0)}' )
+SETUP=RNABCKBQNR
+PLY=19
+while : ; do
+	GROUP=$( echo $COUNTER | awk '{print sprintf("%03d",$0)}' )
+	lunacy64 ./Play.lua \
+		--thread${THREAD} ${SETUP} \
+		$PLY 5 > ${SETUP}-${GROUP}-${PLY}ply.txt
+	if [ "$?" == "1" ] ; then
+	  echo Warning: Exit with non 0 exit code
+	  echo Is capablanca-bb644ef32758.nnue here?
+	fi
+	echo $COUNTER
+	COUNTER=$(( $COUNTER + 1 ))
+done
---
+++ Capa/Tourneys/Finesse/19PlyNNUE-multiThread/tally.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+# Tally xboard interface (HCE) games
+
+FILENAME="$1"
+if [ -z "$FILENAME" ] ; then
+  FILENAME=Finesse12PlyHCE.txt.xz
+fi
+COMMAND=cat
+if echo $FILENAME | grep -F .xz ; then
+  COMMAND=xzcat
+fi
+COUNT="$2"
+if [ -n "$COUNT" ] ; then
+  $COMMAND $FILENAME | fgrep '{' | head -$COUNT > foo-$$
+  FILENAME="foo-$$"
+fi
+
+$COMMAND $FILENAME | fgrep '{' | cut -f2 -d{ | tr -d '\015' | tr -d } | awk '
+{f=$1;if(f == "draw"){f="Draw"}a[f]++;n++}
+END{
+  for(b in a){print b " " a[b] " " a[b]/n*100 "%"}
+  print n " games played";
+  print ((a["White"] - a["Black"]) / n * 100) "% White winning edge"
+  print ((a["White"] + (a["Draw"] / 2)) / n * 100) "% White score"
+  print (a["White"] / (a["White"] + a["Black"]) * 100) "% White decisive wins"
+}'
+
+rm -f foo-$$
